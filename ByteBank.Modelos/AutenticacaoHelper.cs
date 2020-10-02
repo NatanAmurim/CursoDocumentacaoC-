e9b892d53dd1a25e07464066514849e1ceb0c9ef
Natan Amurim <68824563+NatanAmurim@users.noreply.github.com>
2020-10-01 23:17:29 -0300

    Add files via upload

---
+++ ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    //Essa classe pertence somente a biblioteca, por tanto só será vista por membros da biblioteca.
+    //Projetos externos não conseguirão vê-la.
+    //Para ela ter as características citadas acima, a palavra internal deve ser utilizada como seu modificador de acesso.
+    //Obs: Se nenhum modificador for escrito para a classe, ela atribuí o internal como padrão.
+    //internal class AutenticacaoHelper
+    
+    class AutenticacaoHelper
+    {
+        public bool CompararaSenhas(string senhaVerdadeira, string senhaTentativa) 
+        {
+            return senhaVerdadeira == senhaTentativa;
+        }
+    }
+}
---
+++ ByteBank.Modelos/ClasseTeste.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    class ClasseTeste : ModificadoresTeste
+    {
+        public ClasseTeste()
+        {
+            ModificadoresTeste modificadoresTeste = new ModificadoresTeste();
+            modificadoresTeste.MetodoPublico();
+            MetotoProtected();
+
+        }
+    }
+
+    class ModificadoresTeste
+    {
+        public void MetodoPublico()
+        {
+
+        }
+
+        private void MetodoPrivate()
+        {
+
+        }
+
+        protected void MetotoProtected()
+        {
+
+        }
+
+        internal void MetodoInterno() 
+        { 
+            //Visivel apenas do projeto ao qual ele pertence.
+        }
+
+    }
+}
